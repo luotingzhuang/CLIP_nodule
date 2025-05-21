c51fb7f598c1bc5e61bc2730dad1d3b5e3db2731
luotingzhuang <luotingzhuang@outlook.com>
2025-05-21 12:32:43 -0700

    fix warning

--- evaluate.py
+++ evaluate.py
@@ -62,11 +62,11 @@ if __name__ == '__main__':
     outputdf = {}
     for i in range(5):
         all_test_result_i = all_test_result[i].groupby('pid').max().reset_index()
-        outputdf[f'raw_{i}'] =  all_test_result_i.probs
+        outputdf[f'raw_{i}'] =  all_test_result_i.probs.values
             
     outputdf = pd.DataFrame(outputdf)
-    outputdf['ensemble'] = outputdf.mean(1).values
-    outputdf['pid'] = all_test_result_i.pid.values
+    outputdf.loc[:,'ensemble'] = outputdf.mean(1).values
+    outputdf.loc[:,'pid'] = all_test_result_i.pid.values
 
     os.makedirs(eval_args.save_path, exist_ok=True)
     outputdf.to_csv(os.path.join(eval_args.save_path, 'result.csv'), index=False)
